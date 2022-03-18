0e85fba23fb65aa83be072a10f197c0650b28142
EmirBigBoss <r0859222@student.vives.be>
2022-03-18 18:54:21 +0100

    Update Spaties.js

--- JavaScript3/scripts/Spaties.js
+++ JavaScript3/scripts/Spaties.js
@@ -7,6 +7,5 @@ const split = () => {
     let input = document.getElementById("input");
     let inputModi = input.value.split(' ').join('');
     console.log(inputModi.split('').join(' '));
-
 }
 window.addEventListener("load", setup);
