4962e61e2eed53ece85a4492d7947b08663d0558
EmirBigBoss <r0859222@student.vives.be>
2022-03-18 16:33:40 +0100

    Added spaties

---
+++ JavaScript3/scripts/Spaties.js
@@ -0,0 +1,12 @@
+const setup = () => {
+    let button = document.getElementById("button");
+    button.addEventListener("click", split);
+}
+
+const split = () => {
+    let input = document.getElementById("input");
+    let inputModi = input.value.split(' ').join('');
+    console.log(inputModi.split('').join(' '));
+
+}
+window.addEventListener("load", setup);
