75e92ab39a9d81f030a70b1d2bf53bba61c56fcd
EmirBigBoss <r0859222@student.vives.be>
2022-03-29 18:55:56 +0200

    Update trigram.js

--- Javascript5/scripts/trigram.js
+++ Javascript5/scripts/trigram.js
@@ -1,28 +1,18 @@
 const setup = () => {
     let tekst = document.getElementById('trigram').textContent;
-    
+
     let i = 0;
     let stop = false;
     let trigram = '';
 
     while (i < tekst.length && !stop) {
-
-            if(i === tekst.length - 2) {
-                trigram = (tekst[i] + tekst[i+1]);
-                stop = true;
-            }
-            else if(i === tekst.length-1) {
-                trigram = (tekst[i]);
-                stop = true;
-            }
-            else {
-                trigram = (tekst[i] + tekst[i+1] + tekst[i+2]);
-            }
-
-            
+        if(i === tekst.length-3) {
+            stop = true;
+        }
+        trigram = (tekst[i] + tekst[i+1] + tekst[i+2]);
 
         console.log(trigram);
-        i += 3;
+        i += 1;
 
     }
 }
